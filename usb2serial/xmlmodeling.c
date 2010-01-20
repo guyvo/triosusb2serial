addd559a02709341a9fecfa2922ad7a013068808
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-01-20 21:03:58 +0000

    initial add

---
+++ usb2serial/xmlmodeling.c
@@ -0,0 +1,11 @@
+/*
+ *  xmlmodeling.c
+ *  usb2serial
+ *
+ *  Created by Guy Van Overtveldt on 20/01/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "xmlmodeling.h"
+
