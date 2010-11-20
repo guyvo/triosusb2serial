0b05a8edc5477896ea5409c4a0eb66d4ce259d79
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-20 16:39:42 +0000

    initial import

---
+++ trunk/TriosBase/iTrios/Classes/iTriosViewController.h
@@ -0,0 +1,16 @@
+//
+//  iTriosViewController.h
+//  iTrios
+//
+//  Created by Guy Van Overtveldt on 20/11/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iTriosViewController : UIViewController {
+
+}
+
+@end
+
