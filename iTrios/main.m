9a52dfcf80b072883bc6cfafd642a3d38da7a3b2
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-23 19:58:59 +0000

    added trios base modules

--- iTrios/main.m
+++ iTrios/main.m
@@ -3,7 +3,7 @@
 //  iTrios
 //
 //  Created by Guy Van Overtveldt on 20/11/10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Copyright __MyCompanyName__ 2010. All rights reserved. 
 //
 
 #import <UIKit/UIKit.h>
