a686f517b3728bf81f5bb4429a081e72662f949f
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-24 18:18:21 +0000

    added single view

---
+++ trunk/iTrios/TriosSingleView.h
@@ -0,0 +1,16 @@
+//
+//  TriosSingleView.h
+//  iTrios
+//
+//  Created by Guy Van Overtveldt on 24/11/10.
+//  Copyright 2010 ATOS worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TriosSingleView : UIView {
+
+}
+
+@end
