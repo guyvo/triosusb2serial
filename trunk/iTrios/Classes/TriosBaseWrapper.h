f4ad860d005be7223681083cda02677a9a881a8a
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-24 18:12:35 +0000

    added wrapper

---
+++ trunk/iTrios/Classes/TriosBaseWrapper.h
@@ -0,0 +1,16 @@
+//
+//  TriosBaseWrapper.h
+//  iTrios
+//
+//  Created by Guy Van Overtveldt on 24/11/10.
+//  Copyright 2010 ATOS worldline. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TriosBaseWrapper : NSObject {
+
+}
+
+@end
---
+++ trunk/iTrios/Classes/TriosBaseWrapper.m
@@ -0,0 +1,14 @@
+//
+//  TriosBaseWrapper.m
+//  iTrios
+//
+//  Created by Guy Van Overtveldt on 24/11/10.
+//  Copyright 2010 ATOS worldline. All rights reserved.
+//
+
+#import "TriosBaseWrapper.h"
+
+
+@implementation TriosBaseWrapper
+
+@end
