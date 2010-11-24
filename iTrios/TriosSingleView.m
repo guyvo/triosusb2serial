03cdf0be17cea018657bca18eba0aeb12d776988
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-24 18:18:21 +0000

    added single view

---
+++ iTrios/TriosSingleView.m
@@ -0,0 +1,37 @@
+//
+//  TriosSingleView.m
+//  iTrios
+//
+//  Created by Guy Van Overtveldt on 24/11/10.
+//  Copyright 2010 ATOS worldline. All rights reserved.
+//
+
+#import "TriosSingleView.h"
+
+
+@implementation TriosSingleView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
