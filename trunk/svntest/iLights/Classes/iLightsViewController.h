859d2ae1533b1cd645e24cc2412414c021222a91
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-11-27 17:08:25 +0000

    initial

---
+++ trunk/svntest/iLights/Classes/iLightsViewController.h
@@ -0,0 +1,16 @@
+//
+//  iLightsViewController.h
+//  iLights
+//
+//  Created by guy on 27/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iLightsViewController : UIViewController {
+
+}
+
+@end
+
