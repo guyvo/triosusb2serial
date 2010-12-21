7b5f2aecbad6f05bf52397d14c54bd36fcbcd1e5
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2010-12-21 18:26:13 +0000

    bringing in slider code

--- iLights/LightView.m
+++ iLights/LightView.m
@@ -21,9 +21,33 @@
 		self.layer.cornerRadius = VIEW_CORNER_RADIUS;
 		self.layer.borderWidth = VIEW_BORDER_THIKNESS;
 		
+		UISlider * sliderValue = [[UISlider alloc] initWithFrame:CGRectMake(0,0,200,20)];
+		sliderValue.transform = CGAffineTransformScale(sliderValue.transform,2, 1);
+		sliderValue.center = CGPointMake(50, 250);
+		sliderValue.transform = CGAffineTransformRotate(sliderValue.transform, 270.0/180*M_PI);
+		sliderValue.minimumValue = 0.0;
+		sliderValue.maximumValue = 100.0;
+		sliderValue.continuous = YES;
+		sliderValue.value = 50.0;
+		
+		UIImage* thumbImage = [UIImage imageNamed:@"raster.png"];
+		[sliderValue setThumbImage:thumbImage forState:UIControlStateNormal];
+		[sliderValue setThumbImage:thumbImage forState:UIControlStateHighlighted];
+		
+		thumbImage = [UIImage imageNamed:@"yellow100x100.png"];
+		[sliderValue setMinimumTrackImage:thumbImage forState:UIControlStateNormal]; 
+		
+		thumbImage = [UIImage imageNamed:@"grey.png"];
+		[sliderValue setMaximumTrackImage:thumbImage forState:UIControlStateNormal]; 
+		
+		[sliderValue addTarget:(id)self action:@selector(handleSlider:) forControlEvents:UIControlEventValueChanged];
+		
+		[self addSubview:sliderValue];
+		
+		[sliderValue release];
 		
 		UISwipeGestureRecognizer *swiper = [[UISwipeGestureRecognizer alloc]
-													initWithTarget:self action:@selector(handleSlider:)];
+													initWithTarget:self action:@selector(handleSwipeLeft:)];
 		swiper.direction = UISwipeGestureRecognizerDirectionLeft;
 		[self  addGestureRecognizer:swiper];
 
@@ -31,7 +55,11 @@
     return self;
 }
 
-- (IBAction)handleSlider:(UIGestureRecognizer *)sender {
+- (IBAction)handleSlider:(id)sender{
+
+}
+
+- (IBAction)handleSwipeLeft:(UIGestureRecognizer *)sender {
 	
 	for( UIView * view in self.superview.subviews){
 		[view setUserInteractionEnabled:YES];
