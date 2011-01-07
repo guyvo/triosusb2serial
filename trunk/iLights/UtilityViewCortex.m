5f9604f7d92d8b213a51bd2977d5169518f13640
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2011-01-07 10:27:42 +0000

    path tryouts

--- trunk/iLights/UtilityViewCortex.m
+++ trunk/iLights/UtilityViewCortex.m
@@ -66,20 +66,53 @@
 		view.image = [[UIImage alloc]initWithContentsOfFile:[[[NSBundle mainBundle] resourcePath]  
 																		 stringByAppendingPathComponent:@"cortex.png"]];
 		
+		
+		
 		[self addSubview:view];
 		
+		CGMutablePathRef  path = CGPathCreateMutable();
+		
+		CGPathMoveToPoint(path, NULL, 150, 50);
+		CGPathAddLineToPoint(path, NULL, 200, 50);
+		CGPathAddLineToPoint(path, NULL, 200, 0);
+		CGPathAddLineToPoint(path, NULL, 50, 0);
+		CGPathAddLineToPoint(path, NULL, 50, 75);
+		CGPathAddLineToPoint(path, NULL, 150, 50);
+		
+		
+		CAKeyframeAnimation * animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+
+		animation.path = path;
+		animation.duration = 10;
+		animation.repeatCount = 10;
+		animation.removedOnCompletion = YES;
+		animation.fillMode = kCAFillModeForwards;
+		animation.calculationMode = kCAAnimationCubicPaced;
+/*
+		animation.values = [NSArray arrayWithObjects:
+							//[NSNumber numberWithFloat:(0.0 / 180.0) * M_PI],
+							[NSNumber numberWithFloat:(179.0 / 180.0) * M_PI],
+							[NSNumber numberWithFloat:(-179.0 / 180.0) * M_PI], nil];
+		
+*/		
+//		view.layer.anchorPoint = CGPointMake(0,0);
+		[view.layer addAnimation:animation forKey:@"position"];
+		
+		
+		/*
 		[UIView 
 		 animateWithDuration:SWIPE_ANIM_DURATION
 		 delay:0
 		 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat
 		 animations:^{
-			 view.layer.transform = CATransform3DMakeRotation(-M_PI, 1, 1, 1);
+			 view.layer.transform = CATransform3DMakeRotation(M_PI*0.99, 0, 0, 1);
+			 view.layer.transform = CATransform3DMakeRotation(-M_PI*0.99, 0, 0, 1);
 			 
 		 }
 		 completion:^(BOOL finished){
 			 //view.layer.transform = CATransform3DMakeRotation(0, 0, 1, 1);
 		 }];
-		
+		*/
 		return self;
 		
     }
