e6577c6204cca26e3e9d3fbb29ab8c7ff791ffed
guyvo67 <guyvo67@584b68c2-05be-11df-a9fb-49f3208c864b>
2011-01-07 21:27:28 +0000

    signal notifications to the animations still bad access to resolve

--- iLights/UtilityViewCortex.m
+++ iLights/UtilityViewCortex.m
@@ -11,6 +11,67 @@
 
 @implementation UtilityViewCortex
 
+- (void) animateTheView{
+	UIImageView * view = [[UIImageView alloc]initWithFrame:CGRectMake(85, 25, 90, 90)];
+	
+	
+	view.image = [[UIImage alloc]initWithContentsOfFile:[[[NSBundle mainBundle] resourcePath]  
+														 stringByAppendingPathComponent:@"cortex.png"]];
+	
+	view.alpha = 0.6;
+	
+	[self addSubview:view];
+	
+	[self bringSubviewToFront:view];
+	
+	for (int i = 0 ; i< NUMBER_OF_ONES ; i++){
+		
+		CGMutablePathRef  path = CGPathCreateMutable();
+		
+		CGPathMoveToPoint(path, NULL, 0 + (i*5), 75);
+		CGPathAddLineToPoint(path, NULL, 250 + (i*1) , 75);
+		
+		CATextLayer * one = [CATextLayer layer];
+		
+		if ( i % 2 ){
+			one.string = @"1";
+		}else{
+			one.string = @"0";
+		}
+		
+		one.fontSize = 20;
+		one.frame = CGRectMake(0, 0, 20, 20);
+		one.foregroundColor = [[UIColor colorWithHue:0.99 saturation:0.9 brightness:0.9 alpha:0.8]CGColor];
+		
+		CAKeyframeAnimation * animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+		
+		animation.path = path;
+		CGPathRelease(path);
+		animation.duration = 5+(i*0.75);
+		animation.repeatCount = 10000;// must be MAX_INT
+		animation.removedOnCompletion = NO;
+		animation.fillMode = kCAFillModeForwards;
+		animation.calculationMode = kCAAnimationPaced;
+		
+		[one addAnimation:animation forKey:@"position"];
+		[self.layer insertSublayer:one below:view.layer];
+		
+	}
+	
+	[UIView 
+	 animateWithDuration:SWIPE_ANIM_DURATION
+	 delay:0
+	 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat
+	 animations:^{
+		 view.layer.transform = CATransform3DMakeScale(1.3, 1.3, 1);
+		 view.layer.transform = CATransform3DMakeScale(0.3, 0.3, 1);
+	 }
+	 completion:^(BOOL finished){
+	 }];
+	
+	
+}
+
 -(id) initWithIcon:(UIImage *) icon andFrame:(CGRect)frame{
 	[self initWithFrame:frame];
 	if (self != nil){
@@ -25,11 +86,24 @@
 	return nil;
 }
 
-
 - (id)initWithFrame:(CGRect)frame {
     
     self = [super initWithFrame:frame];
     if (self) {
+		NSNotificationCenter * enterBackground = [NSNotificationCenter defaultCenter];
+		
+		[enterBackground addObserver: self
+							selector: @selector (stopAnimating:)
+								name: UIApplicationDidEnterBackgroundNotification
+							  object: nil];
+
+		NSNotificationCenter * enterForeground = [NSNotificationCenter defaultCenter];
+		
+		[enterForeground addObserver: self
+							selector: @selector (startAnimating:)
+								name: UIApplicationWillEnterForegroundNotification
+							  object: nil];
+		
 		self.backgroundColor = [UIColor blackColor];
 		self.layer.borderColor =[[UIColor colorWithRed:0.8 green:0.3 blue:0.2 alpha:1]CGColor];
 		self.layer.cornerRadius = VIEW_CORNER_RADIUS;
@@ -56,68 +130,26 @@
 		[singleFingerSTap release];
 		[singleFingerDTap release];
 		
-		UIImageView * view = [[UIImageView alloc]initWithFrame:CGRectMake(85, 25, 90, 90)];
-		
-		
-		view.image = [[UIImage alloc]initWithContentsOfFile:[[[NSBundle mainBundle] resourcePath]  
-															 stringByAppendingPathComponent:@"cortex.png"]];
-		
-		view.alpha = 0.6;
-		
-		[self addSubview:view];
-		
-		[self bringSubviewToFront:view];
-		
-		
-		for (int i = 0 ; i< NUMBER_OF_ONES ; i++){
-			
-			CGMutablePathRef  path = CGPathCreateMutable();
-			
-			CGPathMoveToPoint(path, NULL, 0 + (i*5), 75);
-			CGPathAddLineToPoint(path, NULL, 250 + (i*1) , 75);
-			
-			CATextLayer * one = [CATextLayer layer];
-			
-			if ( i % 2 ){
-				one.string = @"1";
-			}else{
-				one.string = @"0";
-			}
-			
-			one.fontSize = 20;
-			one.frame = CGRectMake(0, 0, 20, 20);
-			one.foregroundColor = [[UIColor colorWithHue:0.99 saturation:0.9 brightness:0.9 alpha:0.8]CGColor];
-			
-			CAKeyframeAnimation * animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-			
-			animation.path = path;
-			CGPathRelease(path);
-			animation.duration = 5+(i*0.75);
-			animation.repeatCount = 10000;// must be MAX_INT
-			animation.removedOnCompletion = YES;
-			animation.fillMode = kCAFillModeForwards;
-			animation.calculationMode = kCAAnimationPaced;
-			
-			[one addAnimation:animation forKey:@"position"];
-			[self.layer insertSublayer:one below:view.layer];
-			
-		}
-		[UIView 
-		 animateWithDuration:SWIPE_ANIM_DURATION
-		 delay:0
-		 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat
-		 animations:^{
-			 view.layer.transform = CATransform3DMakeScale(1.3, 1.3, 1);
-			 view.layer.transform = CATransform3DMakeScale(0.3, 0.3, 1);
-		 }
-		 completion:^(BOOL finished){
-		 }];
+		[self animateTheView];
+
 		
 		return self;
 		
     }
     return self;
 }
+
+
+
+- (void) stopAnimating:(id) notification{
+	[self.layer removeAllAnimations];
+	
+}
+
+- (void) startAnimating:(id) notification{
+	[self animateTheView];
+}
+
 
 - (IBAction)handleSingleTap:(UIGestureRecognizer *)sender {
 	
